00a5c8d1b354642cd3cb202878fc6a0d4bdee54a
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-08-12 21:43:52 +0530

    department completed

--- inventory/src/pages/Departments.js
+++ inventory/src/pages/Departments.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/DataContext';
+import { useNavigate } from 'react-router';
 
 export default function Departments() {
-    return(
 
+    const{dispatch} = useContext(DataContext)
+    const navigate = useNavigate();
+
+  const navigateDepartmentHandler = (e)=> {
+    dispatch({
+        type: "FILTER_APPLY",
+        payload: { type: "department", value: "e.target.textContent" }
+      });
+      navigate("/products");
+  }
+
+    return(
     
-    <main className="flex items-center justify-center gap-6 px-8 py-12 ">
-    <button className="flex h-max flex-col items-center gap-2 rounded-lg bg-slate-200 p-6 hover:opacity-90">
+    <main className="flex items-center justify-center gap-8 px-8 py-10 ">
+    <button  onClick={navigateDepartmentHandler} className="flex h-max flex-col items-center gap-2 rounded-lg bg-slate-100 p-5 ">
       <p className="text-2xl font-bold">Kitchen</p>
     </button>
 
-    <button className="flex h-max flex-col  items-center gap-2 rounded-lg bg-slate-200 p-6 hover:opacity-90">
+    <button onClick={navigateDepartmentHandler} className="flex h-max flex-col  items-center gap-2 rounded-lg bg-slate-100 p-5 ">
       <p className="text-2xl font-bold">Clothing</p>
     </button>
 
-    <button className="flex h-max flex-col items-center gap-2 rounded-lg bg-slate-200 p-6">
-      <p className="text-2xl font-bold hover:opacity-90">Toys</p>
+    <button  onClick={navigateDepartmentHandler} className="flex h-max flex-col items-center gap-2 rounded-lg bg-slate-100 p-5">
+      <p className="text-2xl font-bold ">Toys</p>
     </button>
   </main>
     )
--- inventory/src/pages/Products.js
+++ inventory/src/pages/Products.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 
 export default function Products() {
+    const {state: {inventoryData}} = useContext(DataContext)
+    console.log(inventoryData)
     
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <main className="px-4 py-8">
+          <div className="mr-2 flex items-center justify-between p-2">
+            <h2 className="text-3xl font-bold">Products</h2>
+    
+            <select
+              name="department"
+              id="department"
+              className="rounded border-2 border-gray-700 px-[5px]"
+            >
+              <option value="All">All Departments</option>
+              <option value="Kitchen">Kitchen</option>
+              <option value="Clothing">Clothing</option>
+              <option value="Toys">Toys</option>
+            </select>
+    
+            <div className="flex items-center gap-2">
+              <input type="checkbox" name="stock" id="low-stock" />
+              <label htmlFor="low-stock" className="text-lg">
+                Low Stock Item
+              </label>
+            </div>
+    
+            <select
+              name="sort"
+              id="sort"
+              className="rounded border-2 border-gray-700 px-[5px]"
+            >
+              <option value="name">Name</option>
+              <option value="price">Price</option>
+              <option value="stock">Stock</option>
+            </select>
+    
+            
+          </div>
+    
+          <div>
+            <table className="min-w-full">
+              <thead>
+                <tr className="border-b bg-gray-300">
+                  <th className="px-4 py-2 text-center">Image</th>
+                  <th className="px-4 py-2 text-center">Name</th>
+                  <th className="px-4 py-2 text-center">Description</th>
+                  <th className="px-4 py-2 text-center">Price</th>
+                  <th className="px-4 py-2 text-center">Stock</th>
+                  <th className="px-4 py-2 text-center">Supplier</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inventoryData.map((item) => (
+                  <tr key={item.id} className="border-b text-center">
+                    <td className="px-4 py-2">
+                      <img
+                        src={item.imageUrl}
+                        alt={item.name}
+                        className="h-16"
+                      />
+                    </td>
+                    <td className="px-4 py-2">{item.name}</td>
+                    <td className="px-4 py-2">{item.description}</td>
+                    <td className="px-4 py-2">${item.price}</td>
+                    <td className="px-4 py-2">{item.stock}</td>
+                    <td className="px-4 py-2">{item.supplier}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </main>
+      );
+    }
+    
